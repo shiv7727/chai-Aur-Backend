ae965c2a5df4b2a93653a4b044c30a0509139678
shiv gaurav <shivgaurav27@gmail.com>
2024-12-08 22:38:54 +0530

    add new routes for usert controller

--- src/routes/user.routes.mjs
+++ src/routes/user.routes.mjs
@@ -1,8 +1,14 @@
 import Router from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.mjs";
 import { upload } from "../middlewares/multer.middleware.mjs";
 import { verifyJWT } from "../middlewares/Auth.middleware.mjs";
@@ -23,6 +29,17 @@ router.route("/login").post(loginUser);
 // secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refreshToken").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
